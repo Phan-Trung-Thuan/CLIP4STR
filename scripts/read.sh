f580e736d91f6890a2946fc0705e652d5f15a5f1
Phan Trung Thuận <143245984+Phan-Trung-Thuan@users.noreply.github.com>
2025-01-16 09:23:01 +0700

    Update read.sh

--- scripts/read.sh
+++ scripts/read.sh
@@ -4,9 +4,9 @@ ckpt_id=$2
 images_path=$3
 
 ### DEFINE THE ROOT PATH HERE ###
-abs_root=/PUT/YOUR/PATH/HERE
+abs_root=/kaggle/working/CLIP4STR
 
-exp_path=${abs_root}/output/${ckpt_id}
-runfile=../read.py
+exp_path=${abs_root}/${ckpt_id}
+runfile=read.py
 
 python ${runfile} ${exp_path} --images_path ${images_path}
