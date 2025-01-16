07c74fe61680bcca24a2fa4a84b9bf2aa21cdfb4
Phan Trung Thuận <143245984+Phan-Trung-Thuan@users.noreply.github.com>
2025-01-16 09:41:17 +0700

    Update system.py

--- strhub/models/vl_str/system.py
+++ strhub/models/vl_str/system.py
@@ -20,7 +20,7 @@ from .modules import DecoderLayer, Decoder, modify_attn_mask
 
 # an alternative choice when the input argument is not valid 
 # CLIP_PATH = '/PUT/YOUR/PATH/HERE/pretrained/clip''
-CLIP_PATH = '/home/shuai/pretrained/clip'
+CLIP_PATH = '/kaggle/working/CLIP4STR'
 if not os.path.exists(CLIP_PATH):
     CLIP_PATH = '/home/shuzhao/Data/pretrained/clip'
 assert os.path.exists(CLIP_PATH)
